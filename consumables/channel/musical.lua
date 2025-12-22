e6c1ca784f353d9d9d0e7c0122432b04d803ab61
spencers145 <smartypantsspencer@gmail.com>
2025-12-21 19:12:56 -0500

    Uhhh dont break musical too

--- consumables/channel/musical.lua
+++ consumables/channel/musical.lua
@@ -9,7 +9,7 @@ SMODS.Consumable {
         },
     },
     use = function(self, card, area)
-        for i = 1, math.min(card.ability.extra.channels, ) do
+        for i = 1, math.min(card.ability.extra.channels, G.consumeables.config.card_limit - #G.consumeables.cards) do
             G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.4, func = function()
                 if G.consumeables.config.card_limit > #G.consumeables.cards then
                     play_sound('timpani')
