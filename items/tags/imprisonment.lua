68f38c7567cedcbfe7d9b52925b706e65d4a4a6b
Ruby <138878489+lord-ruby@users.noreply.github.com>
2026-01-16 20:04:53 +0000

    Update imprisonment.lua

--- items/tags/imprisonment.lua
+++ items/tags/imprisonment.lua
@@ -12,6 +12,10 @@ SMODS.Tag {
         return false
     end,
     apply = function(self, tag, context)
+        if context.type == 'real_round_before_start' and not (next(SMODS.find_card("j_bld_taglock")) and not (G.GAME.blind.boss or G.GAME.last_joker)) and G.GAME.blind.boss then
+            G.GAME.blind:disable()
+            G.GAME.blindassist:disable()
+        end
         if not G.GAME.imprisonment_buffer and context.type == 'real_round_start' and not (next(SMODS.find_card("j_bld_taglock")) and not (G.GAME.blind.boss or G.GAME.last_joker)) and G.GAME.blind.boss then
             G.GAME.imprisonment_buffer = true
             G.E_MANAGER:add_event(Event({
