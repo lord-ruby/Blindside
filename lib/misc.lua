10296cbbf7bb0b69e386e1d9577b7bf83953827a
Ruby <138878489+lord-ruby@users.noreply.github.com>
2026-01-16 18:33:51 +0000

    add tag compression code from cryptid

--- lib/misc.lua
+++ lib/misc.lua
@@ -31,3 +31,34 @@ function Game:main_menu(change_context)
     })
     G.SETTINGS.tutorial_complete = true
 end
+
+--PROPERLY COMPRESS TAGS
+local at = add_tag
+function add_tag(tag)
+	at(tag)
+	-- Update Costs for Clone Tag
+	if tag.name == "cry-Clone Tag" then
+		for k, v in pairs(G.I.CARD) do
+			if v.set_cost then
+				v:set_cost()
+			end
+		end
+	end
+	-- Make tags fit if there's more than 13 of them
+	-- This + Tag.remove Hook modify the offset to squeeze in more tags when needed
+	if #G.HUD_tags > 13 then
+		for i = 2, #G.HUD_tags do
+			G.HUD_tags[i].config.offset.y = 0.9 - 0.9 * 13 / #G.HUD_tags
+		end
+	end
+end
+
+local tr = Tag.remove
+function Tag:remove()
+	tr(self)
+	if #G.HUD_tags >= 13 then
+		for i = 2, #G.HUD_tags do
+			G.HUD_tags[i].config.offset.y = 0.9 - 0.9 * 13 / #G.HUD_tags
+		end
+	end
+end
